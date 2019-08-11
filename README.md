5a1b15ebbbf635ca670ea73ef3702d91a67e6586
Wes Baker <wes@wesbaker.com>
2019-08-11 15:52:00 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Alfred D&D Beyond Workflow
+
+Search D&D Beyond from the comfort of Alfred.
+
+![Workflow in action](media/example.png)
+
+## Install
+
+```shell
+$ npm install --global alfred-dndbeyond-workflow
+```
+
+*Requires [Node.js](https://nodejs.org) 6+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
+
+## Usage
+
+In Alfred, type `dnd` then your query to search for D&D Beyond books. Hit 
+<kbd>Enter</kbd> to go to the given D&D Beyond page.
+
+## License
+
+MIT © [Wes Baker](http://wesbaker.com)
---
+++ index.js
@@ -0,0 +1,27 @@
+const alfy = require("alfy");
+const querystring = require("querystring");
+
+alfy
+  .fetch("https://www.dndbeyond.com/es/term", {
+    query: { query: alfy.input }
+  })
+  .then(suggestions => {
+    // No results
+    if (suggestions["list"].length === 0) {
+      return alfy.output([
+        {
+          title: `No suggestions found for '${alfy.input}'`,
+          arg: "https://www.dndbeyond.com/search"
+        }
+      ]);
+    }
+
+    const searches = suggestions["list"].map(suggestion => {
+      const cleaned = suggestion.replace(/'/g, "%27");
+      return {
+        title: suggestion,
+        arg: `https://www.dndbeyond.com/search?q=${cleaned}`
+      };
+    });
+    alfy.output(searches);
+  });
